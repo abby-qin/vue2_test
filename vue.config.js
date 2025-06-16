cb514bbe22bf516070902e10f44722facd674032
abby-qin <abbyqin0704@163.com>
2025-06-16 23:39:28 +0800

    添加

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false // 关闭ESLint检查
+  lintOnSave: false, // 关闭ESLint检查，
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue2_test/'  // 必须与你的仓库名一致（区分大小写）
+    : '/'
 });
